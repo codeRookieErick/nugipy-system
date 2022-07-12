1a0c3547ef505577ed7691496c4d843be2535f4a
codeRookieErick <erickmoradev@gmail.com>
2022-07-12 13:59:22 -0400

    Reading uploaded nuget

--- router_for_publish.py
+++ router_for_publish.py
@@ -1,14 +1,32 @@
 from threadsnake.core import *
+from threading import Thread
+import zipfile
 
 app:Router = export(__name__)
 
+def upload_nuget(location:str):
+    try:
+        fileLocation:str = location['tempFilePath']
+        with zipfile.ZipFile(fileLocation, 'r') as file:
+            files = [i for i in file.namelist()]
+            nuspecFileCandidates = [i for i in files if i.endswith('.nuspec') and '/' not in i]
+            if len(nuspecFileCandidates) > 0:
+                nuspecFileName = nuspecFileCandidates[0]
+                nuspecFileContent = ''
+                with file.open(nuspecFileName) as nuspecFile:
+                    nuspecFileContent = nuspecFile.read().decode('latin1')
+            print(nuspecFileContent, files)
+    except Exception as e:
+        print(e)
 
 #https://docs.microsoft.com/en-us/nuget/api/package-publish-resource
 #@app.register_function('PUT', '/{package:re([\w_\-.]+)?}')
 @app.register_function('PUT', '/')
 #@validates_header('X-NuGet-ApiKey', lambda a: a == a)
 def publish_put(app:Application, req:HttpRequest, res:HttpResponse):
-    print(req.files)
+    for r in req.files:
+        Thread(target=lambda: upload_nuget(req.files[r])).start()
+    res.end("OK", 200)
 
 @app.register_function('DELETE', '/{id}.{version}.nupkg/{id2}/{version2}')
 @requires_parameters(['id', 'version', 'id2', 'version2'])
